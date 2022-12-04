9f8b219a1dec8370c84739af3ff4c69a05bfccd6
iykenny <imanziyannickkenny@gmail. com>
2022-12-04 09:58:44 +0200

    second commit

---
+++ Audite/src/main/java/Audite/CRUD/Audite/AuditeApplication.java
@@ -0,0 +1,13 @@
+package Audite.CRUD.Audite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditeApplication.class, args);
+	}
+
+}
---
+++ Audite/src/test/java/Audite/CRUD/Audite/AuditeApplicationTests.java
@@ -0,0 +1,13 @@
+package Audite.CRUD.Audite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
